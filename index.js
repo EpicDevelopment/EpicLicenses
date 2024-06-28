0a53e97daded407be18b0e0f96286bc505af7a4c
snowy <133208096+snowy-cool@users.noreply.github.com>
2024-06-28 23:36:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // index.js
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -11,20 +12,22 @@ const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/licenses';
 const discordToken = process.env.DISCORD_TOKEN;
 
 // MongoDB connection
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
 
 // Middleware
 app.use(bodyParser.json());
 
 // Express route to check license
-app.post('/checkLicense', async (req, res) => {
+app.post('/check-license', async (req, res) => {
     const { licenseKey, product } = req.body;
     const License = require('./models/License');
 
     try {
         const license = await License.findOne({ licenseKey, product });
         if (license && !license.blacklisted) {
-            res.json({ status_id: "SUCCESS" });
+            res.json({ status_id: "SUCCESS", license });
         } else {
             res.json({ status_id: "FAILURE" });
         }
